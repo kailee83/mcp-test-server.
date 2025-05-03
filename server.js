02175d9e0075081aab19135ed84d76710a07ddad
kailee83 <83782593+kailee83@users.noreply.github.com>
2025-05-03 09:36:37 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
